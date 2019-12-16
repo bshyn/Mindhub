f807e64a4f74887fb7210d6884174cc3c43d111a
bshyn <52586606+bshyn@users.noreply.github.com>
2019-12-16 12:25:03 -0300

    agrega los partidos faltantes y funcion para resetear

--- tarea9/public/js/main.js
+++ tarea9/public/js/main.js
@@ -1,37 +1,48 @@
-class Game{
+class Game {
   date;
   teams;
   location;
   time;
 
-  constructor(date, teamOne, teamTwo, location, time){
+  constructor(date, teamOne, teamTwo, location, time) {
     this.date = date;
     this.teams = teamOne + '&' + teamTwo;
     this.location = location;
     this.time = time;
   }
 
-  getTeams(){
+  getTeams() {
     return this.teams.split('&');
   }
 }
 
-//September games
-let game1 = new Game('9/01', 'U1', 'U4', 'AJ Katzenmaier', '9:30 a.m.');
-let game2 = new Game('9/01', 'U3', 'U2', 'Greenbay', '1:00 p.m.');
+let games = [
+  new Game('9/01', 'U1', 'U4', 'AJ Katzenmaier', '9:30 a.m.'),
+  new Game('9/01', 'U3', 'U2', 'Greenbay', '1:00 p.m.'),
 
-let game3 = new Game('9/08', 'U5', 'U6', 'Howard A Yeager', '9:30 a.m.');
-let game4 = new Game('9/08', 'U6', 'U1', 'Marjorie P Hart', '1:00 p.m.');
+  new Game('9/08', 'U5', 'U6', 'Howard A Yeager', '9:30 a.m.'),
+  new Game('9/08', 'U6', 'U1', 'Marjorie P Hart', '1:00 p.m.'),
 
-let game5 = new Game('9/15', 'U2', 'U4', 'North', '9:30 a.m.' );
-let game6 = new Game('9/15', 'U3', 'U5', 'AJ Katzenmaier', '1:00 p.m.');
+  new Game('9/15', 'U2', 'U4', 'North', '9:30 a.m.'),
+  new Game('9/15', 'U3', 'U5', 'AJ Katzenmaier', '1:00 p.m.'),
 
-let game7 = new Game('9/22', 'U1', 'U3', 'South', '9:30 a.m.');
-let game8 = new Game('9/22', 'U2', 'U6', 'Howard A Yeager', '1:00 p.m.');
+  new Game('9/22', 'U1', 'U3', 'South', '9:30 a.m.'),
+  new Game('9/22', 'U2', 'U6', 'Howard A Yeager', '1:00 p.m.'),
 
-let game9 = new Game('9/29', 'U4', 'U5', 'Greenbay', '9:30 a.m.');
+  new Game('9/29', 'U4', 'U5', 'Greenbay', '9:30 a.m.'),
 
-let games = [game1, game2, game3, game4, game5, game6, game7, game8, game9];
+  new Game('10/06', 'U2', 'U5', 'Marjorie P Hart', '9:30 a.m.'),
+  new Game('10/06', 'U1', 'U6', 'South', '1:00 p.m.'),
+
+  new Game('10/08', 'U3', 'U4', 'Howard A Yeager', '9:30 a.m.'),
+  new Game('10/08', 'U5', 'U1', 'Greenbay', '1:00 p.m.'),
+
+  new Game('10/20', 'U6', 'U3', 'North', '9:30 a.m.'),
+  new Game('10/20', 'U2', 'U4', 'Marjorie P Hart', '1:00 p.m.'),
+
+  new Game('10/27', 'U3', 'U1', 'AJ Katzenmaier', '9:30 a.m.'),
+  new Game('10/27', 'U5', 'U6', 'Howard A Yeager', '1:00 p.m.')
+];
 
 var app = new Vue({
   el: '#app',
@@ -43,7 +54,7 @@ var app = new Vue({
 
 //Get games by team
 
-function getGamesByTeam(team){
+function getGamesByTeam(team) {
   let result = games.filter(
     game => game.getTeams().includes(team)
   )
@@ -51,7 +62,7 @@ function getGamesByTeam(team){
   app.show = true;
 }
 
-function getGamesByDate(date){
+function getGamesByDate(date) {
   let result = games.filter(
     game => game.date === date
   )
@@ -59,10 +70,16 @@ function getGamesByDate(date){
   app.show = true;
 }
 
-function getGamesByLocation(location){
+function getGamesByLocation(location) {
   let result = games.filter(
     game => game.location === location
   )
   app.games = result;
   app.show = true;
+}
+
+function reset() {
+  app.games = [];
+  app.show = false;
+  $('.navbar-collapse').collapse('hide');
 }
